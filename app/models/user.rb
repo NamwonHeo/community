6be210a1cfd092431293b7ec8e031b27f680251e
Namwon Heo <namwonheo89@gmail.com>
2020-01-05 14:35:37 +0900

    basic posts made adn role added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
+  after_create :assign_default_role
+  rolify
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
+
+  has_many :posts, dependent: :destroy
+
+  def assign_default_role
+    add_role(:basic)
+  end
 end
